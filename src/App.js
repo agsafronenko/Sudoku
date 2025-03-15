c18176603c64d48056f235d245c80590b0d15f3f
Alexander Safronenko <agsafronenko@gmail.com>
2025-03-15 19:40:45 +0100

    Add base files: App.js main component and index.js including styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import GameBoard from './components/GameBoard';
+import SolverPage from './components/SolverPage';
+import Header from './components/Header';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('game');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header 
+        currentPage={currentPage} 
+        onNavigate={setCurrentPage} 
+      />
+      
+      <div className="main-content">
+        {currentPage === 'game' ? (
+          <GameBoard />
+        ) : (
+          <SolverPage />
+        )}
+      </div>
     </div>
   );
 }
