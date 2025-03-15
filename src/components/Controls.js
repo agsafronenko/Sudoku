f6cbe3d32c314eddc4459a9cf1aeb62f37189177
Alexander Safronenko <agsafronenko@gmail.com>
2025-03-15 21:14:43 +0100

    Controls component and styles

---
+++ src/components/Controls.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Controls.css";
+
+function Controls({ isNoteMode, toggleNoteMode, hintsRemaining, incorrectChecksRemaining, useHint, showIncorrectCells, startNewGame }) {
+  return (
+    <div className="controls-container">
+      <button className={`control-button ${isNoteMode ? "active" : ""}`} onClick={toggleNoteMode}>
+        {isNoteMode ? "Pencil Mode" : "Pen Mode"}
+      </button>
+
+      <button className="control-button hint" onClick={useHint} disabled={hintsRemaining <= 0}>
+        Hint {hintsRemaining > 0 && `(${hintsRemaining})`}
+      </button>
+
+      <button className="control-button check" onClick={showIncorrectCells} disabled={incorrectChecksRemaining <= 0}>
+        Check Mistakes {incorrectChecksRemaining > 0 && `(${incorrectChecksRemaining})`}
+      </button>
+
+      <button className="control-button new-game" onClick={startNewGame}>
+        New Game
+      </button>
+    </div>
+  );
+}
+
+export default Controls;
