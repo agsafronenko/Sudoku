fe5431f66acf0738dd6e171e769bbca9e8348b92
Alexander Safronenko <agsafronenko@gmail.com>
2025-03-16 06:39:41 +0100

    Change position of "erase" button

--- src/components/NumberPad.js
+++ src/components/NumberPad.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import './NumberPad.css';
+import React from "react";
+import "./NumberPad.css";
 
 function NumberPad({ onNumberSelect }) {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
   return (
     <div className="number-pad">
-      {numbers.map(num => (
-        <button 
-          key={num} 
-          className="number-button"
-          onClick={() => onNumberSelect(num)}
-        >
-          {num}
+      {numbers.map((num) => (
+        <button key={num} className={`number-button ${num === 0 ? "erase" : ""}`} onClick={() => onNumberSelect(num)}>
+          {num === 0 ? <span className="icon">✕</span> : num}
         </button>
       ))}
-      <button 
-        className="number-button erase"
-        onClick={() => onNumberSelect(0)}
-      >
-        <span className="icon">✕</span>
-      </button>
     </div>
   );
 }
